93de4c02a693ae50df97e772961aa975fa08b5d1
Rodrigo Silva <rodrigo.92.srb@gmail.com>
2025-07-01 14:51:59 -0300

    chore: initialize Spring Boot project with required dependencies

---
+++ src/main/java/com/ajxtech/cruzeiroec/CruzeiroecApiApplication.java
@@ -0,0 +1,13 @@
+package com.ajxtech.cruzeiroec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CruzeiroecApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruzeiroecApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ajxtech/cruzeiroec/CruzeiroecApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajxtech.cruzeiroec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruzeiroecApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
